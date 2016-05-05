2e1e8c2a8e6a906207958acee67256f9f491f374
Daniel Bustorff Silva <dbustorff@hotmail.com>
2016-05-05 15:55:48 -0300

    Rehashed layouts, started metronome

---
+++ app/src/main/java/com/prograstinando/daniel/drumhelper/RudimentMetronome.java
@@ -0,0 +1,13 @@
+package com.prograstinando.daniel.drumhelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RudimentMetronome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rudiment_metronome);
+    }
+}
