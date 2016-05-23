a6e74a18e307d301265b0f0ab844d8ac0a58e499
Daniel Bustorff Silva <dbustorff@hotmail.com>
2016-05-23 13:07:23 -0300

    Started metronome logic

--- app/src/main/java/com/prograstinando/daniel/drumhelper/Metronome.java
+++ app/src/main/java/com/prograstinando/daniel/drumhelper/Metronome.java
@@ -1,24 +1,42 @@
 package com.prograstinando.daniel.drumhelper;
 
+import android.content.DialogInterface;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View.OnClickListener;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Metronome extends AppCompatActivity {
 
-
+    Handler metronomeHandler = new Handler();
+    Button buttonPlus1, buttonPlus5, buttonPlus10, buttonMinus1, buttonMinus5, buttonMinus10;
+    Button buttonTempo;
+    private TextView resultText;
+    int tempo = 60;// = Integer.parseInt(tempoText.getText().toString());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_metronome);
+        buttonTempo = (Button) findViewById(R.id.buttonTempo);
+        buttonTempo.setText(Integer.toString(tempo));
+        buttonTempo.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                inputTempo();
+            }
+        });
     }
 
-    //EditText tempoText = (EditText) findViewById(R.id.editTextTempo);
-    int tempo = 60;// = Integer.parseInt(tempoText.getText().toString());
+    //EditText tempoText = (EditText) findViewById(R.id.editTextTempo)
 
     public void alterTempo(View view){
-        /*int aux;
+        int aux;
         switch (view.getId()){
             case R.id.buttonPlusOne:
                 aux = 1;
@@ -43,6 +61,30 @@ public class Metronome extends AppCompatActivity {
                 break;
         }
         tempo += aux;
-        tempoText.setText(Integer.toString(tempo));*/
+        buttonTempo.setText(Integer.toString(tempo));
     }
+
+    protected void inputTempo(){
+        LayoutInflater layoutInflater = LayoutInflater.from(Metronome.this);
+        View promptView = layoutInflater.inflate(R.layout.activity_input_tempo,null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(Metronome.this);
+        alertDialogBuilder.setView(promptView);
+        final EditText editText = (EditText) promptView.findViewById(R.id.tempoDialogInput);
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        resultText.setText(editText.getText());
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
 }
